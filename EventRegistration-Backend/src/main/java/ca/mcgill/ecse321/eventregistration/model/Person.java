9803fca13fab37f89183b1daae9fe6621bc6fa98
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2024-09-26 10:08:26 -0400

    Write model classes

---
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Person.java
@@ -0,0 +1,32 @@
+package ca.mcgill.ecse321.eventregistration.model;
+
+import java.sql.Date;
+
+public class Person {
+	private int id;
+	private String name;
+	private String emailAddress;
+	private Date creationDate;
+
+	public Person(String name, String emailAddress, Date creationDate) {
+		this.name = name;
+		this.emailAddress = emailAddress;
+		this.creationDate = creationDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public Date getCreationDate() {
+		return creationDate;
+	}
+}
