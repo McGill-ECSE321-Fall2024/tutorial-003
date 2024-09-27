aa2f9bfad5b8dbb57bdd0fdd4ca0588b31926372
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2024-09-27 15:22:53 -0400

    Add JPA annotations

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Registration.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Registration.java
@@ -1,19 +1,53 @@
 package ca.mcgill.ecse321.eventregistration.model;
 
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Registration {
-	private Person registrant;
-	private Event event;
+	@EmbeddedId
+	private Key key;
 
-	public Registration(Person registrant, Event event) {
-		this.registrant = registrant;
-		this.event = event;
-	}
+	// See
+	// https://docs.jboss.org/hibernate/orm/6.5/userguide/html_single/Hibernate_User_Guide.html#identifiers-composite
+	// for the rules on composite IDs.
+	@Embeddable
+	public static class Key implements Serializable {
+		@ManyToOne
+		private Person registrant;
+		@ManyToOne
+		private Event event;
 
-	public Person getRegistrant() {
-		return registrant;
-	}
+		public Key() {
+			super();
+		}
 
-	public Event getEvent() {
-		return event;
+		public Person getRegistrant() {
+			return registrant;
+		}
+
+		public Event getEvent() {
+			return event;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (!(obj instanceof Key)) {
+				return false;
+			}
+			Key that = (Key) obj;
+			return this.getRegistrant().getId() == that.getRegistrant().getId()
+					&& this.getEvent().getId() == that.getEvent().getId();
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(this.getRegistrant().getId(), this.getEvent().getId());
+		}
 	}
 }
