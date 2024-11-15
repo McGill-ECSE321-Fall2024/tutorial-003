acc917625cb3d3d6563ef444f585cf6ef110d78d
Louis Hildebrand <louis.hildebrand@mail.mcgill.ca>
2024-11-15 15:07:27 -0500

    Make events view dynamic

--- EventRegistration-Frontend/src/views/EventsView.vue
+++ EventRegistration-Frontend/src/views/EventsView.vue
@@ -3,19 +3,18 @@
 		<h1>Event Registration</h1>
 		<h2>New Event</h2>
 		<div>
-			<select>
+			<select v-model="newEventType">
 				<option value="IN_PERSON">In person</option>
 				<option value="ONLINE">Online</option>
 			</select>
-			<input type="text" placeholder="Name" />
-			<input type="date" placeholder="Date" />
-			<input type="time" placeholder="Start Time" />
-			<input type="time" placeholder="End Time" />
-			<input type="text" placeholder="Registration Limit" />
-			<input type="text" placeholder="Location" />
-			<button id="create-btn">Create Event</button>
-			<button class="danger-btn">Clear</button>
-			<button class="danger-btn">Delete All Events</button>
+			<input type="text" placeholder="Name" v-model="newEventName" />
+			<input type="date" placeholder="Date" v-model="newEventDate" />
+			<input type="time" placeholder="Start Time" v-model="newEventStartTime" />
+			<input type="time" placeholder="End Time" v-model="newEventEndTime" />
+			<input type="text" placeholder="Registration Limit" v-model="newEventRegLimit" />
+			<input type="text" placeholder="Location" v-model="newEventLocation" />
+			<button id="create-btn" @click="createEvent" v-bind:disabled="!isEventValid()">Create Event</button>
+			<button class="danger-btn" @click="clearInputs">Clear</button>
 		</div>
 		<h2>Events</h2>
 		<table>
@@ -23,12 +22,12 @@
 				<tr>
 					<th>Name</th>
 					<th>Date</th>
-					<th>Limit</th>
+					<th>Type</th>
 				</tr>
-				<tr>
-					<td>Party</td>
-					<td>2024-03-23</td>
-					<td>1</td>
+				<tr v-for="e in events">
+					<td>{{ e.name }}</td>
+					<td>{{ e.date }}</td>
+					<td>{{ e.type === "IN_PERSON" ? "In person" : "Online" }}</td>
 				</tr>
 			</tbody>
 		</table>
@@ -36,7 +35,55 @@
 </template>
 
 <script>
-
+export default {
+	name: "events",
+	data() {
+		return {
+			events: [
+				{ type: "IN_PERSON", name: "Tim's Birthday Party", date: "2024-11-15", startTime: "13:35:00", endTime: "15:25:00", registrationLimit: 0, location: "Tim's house" },
+				{ type: "ONLINE", name: "Online event", date: "2024-11-16", startTime: "12:00:00", endTime: "13:00:00", registrationLimit: 100, location: "Zoom" },
+			],
+			newEventType: "IN_PERSON",
+			newEventName: null,
+			newEventDate: null,
+			newEventStartTime: null,
+			newEventEndTime: null,
+			newEventRegLimit: null,
+			newEventLocation: null
+		};
+	},
+	methods: {
+		createEvent() {
+			const newEvent = {
+				type: this.newEventType,
+				name: this.newEventName,
+				date: this.newEventDate,
+				startTime: this.newEventStartTime,
+				endTime: this.newEventEndTime,
+				registrationLimit: this.newEventRegLimit,
+				location: this.newEventLocation
+			};
+			this.events.push(newEvent);
+			this.clearInputs()
+		},
+		clearInputs() {
+			this.newEventType = "IN_PERSON";
+			this.newEventName = null;
+			this.newEventDate = null;
+			this.newEventStartTime = null;
+			this.newEventEndTime = null;
+			this.newEventRegLimit = null;
+			this.newEventLocation = null;
+		},
+		isEventValid() {
+			return this.newEventName
+				&& this.newEventDate
+				&& this.newEventStartTime
+				&& this.newEventRegLimit
+				&& this.newEventLocation;
+		}
+	}
+};
 </script>
 
 <style>
